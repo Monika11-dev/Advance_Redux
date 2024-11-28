83dcf085ecb3edee9fe1209136fff40ac230ab00
Monika11-dev <monika.chauhan@celestialsys.com>
2024-11-28 17:39:32 +0530

    Added cart functionality using Redux tollkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,26 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
+import { useSelector } from 'react-redux';
+
 
 const CartButton = (props) => {
+
+  const noOfItems = useSelector(state => state.cart.items.length);
+
+  const dispatch = useDispatch();
+
+  const cartHandler = () => {
+
+    dispatch(uiActions.toggle());
+
+  }
+
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={cartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{noOfItems}</span>
     </button>
   );
 };
